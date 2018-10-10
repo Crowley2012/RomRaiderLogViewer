2b40579223cb43499e387c54aa7804d950e37251
Sean Crowley <crowley.p.sean@gmail.com>
2018-10-10 14:37:43 -0500

    Added ability to load grid and graph specific data

--- RomRaiderLogViewer/RomRaiderLogViewer/Forms/MainForm.Designer.cs
+++ RomRaiderLogViewer/RomRaiderLogViewer/Forms/MainForm.Designer.cs
@@ -28,51 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnOpen = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.lblFileName = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.lblBuild = new System.Windows.Forms.ToolStripStatusLabel();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.btnBuildGraph = new System.Windows.Forms.Button();
+            this.cmbGridType = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
+            this.btnOpen});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1424, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // fileToolStripMenuItem
+            // btnOpen
             // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openToolStripMenuItem,
-            this.exitToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
-            // 
-            // openToolStripMenuItem
-            // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
-            this.openToolStripMenuItem.Text = "Open";
-            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(48, 20);
+            this.btnOpen.Text = "Open";
+            this.btnOpen.Click += new System.EventHandler(this.Open_Click);
             // 
             // statusStrip1
             // 
@@ -80,9 +87,9 @@
             this.lblFileName,
             this.toolStripStatusLabel1,
             this.lblBuild});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 777);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(1424, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -94,7 +101,7 @@
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(751, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(1375, 17);
             this.toolStripStatusLabel1.Spring = true;
             // 
             // lblBuild
@@ -103,22 +110,172 @@
             this.lblBuild.Size = new System.Drawing.Size(34, 17);
             this.lblBuild.Text = "Build";
             // 
-            // Form1
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(3, 16);
+            this.chart.Name = "chart1";
+            this.chart.Size = new System.Drawing.Size(1123, 349);
+            this.chart.TabIndex = 2;
+            this.chart.Text = "chart1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1424, 375);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1424, 753);
+            this.splitContainer1.SplitterDistance = 375;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer2.Size = new System.Drawing.Size(1424, 374);
+            this.splitContainer2.SplitterDistance = 285;
+            this.splitContainer2.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox1.Controls.Add(this.btnBuildGraph);
+            this.groupBox1.Controls.Add(this.cmbGridType);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 368);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Graph Settings";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.checkedListBox, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(9, 46);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(264, 287);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox.FormattingEnabled = true;
+            this.checkedListBox.Location = new System.Drawing.Point(3, 3);
+            this.checkedListBox.Name = "checkedListBox1";
+            this.checkedListBox.Size = new System.Drawing.Size(258, 281);
+            this.checkedListBox.Sorted = true;
+            this.checkedListBox.TabIndex = 2;
+            // 
+            // btnBuildGraph
+            // 
+            this.btnBuildGraph.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuildGraph.Location = new System.Drawing.Point(9, 339);
+            this.btnBuildGraph.Name = "btnBuildGraph";
+            this.btnBuildGraph.Size = new System.Drawing.Size(264, 23);
+            this.btnBuildGraph.TabIndex = 1;
+            this.btnBuildGraph.Text = "Build Graph";
+            this.btnBuildGraph.UseVisualStyleBackColor = true;
+            this.btnBuildGraph.Click += new System.EventHandler(this.BuildGraph_Click);
+            // 
+            // cmbGridType
+            // 
+            this.cmbGridType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbGridType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGridType.FormattingEnabled = true;
+            this.cmbGridType.Location = new System.Drawing.Point(9, 19);
+            this.cmbGridType.Name = "cmbGridType";
+            this.cmbGridType.Size = new System.Drawing.Size(264, 21);
+            this.cmbGridType.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.chart);
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1129, 368);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Graph";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1424, 799);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Log Viewer";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,13 +284,21 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem btnOpen;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel lblFileName;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel lblBuild;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBuildGraph;
+        private System.Windows.Forms.ComboBox cmbGridType;
+        private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- RomRaiderLogViewer/RomRaiderLogViewer/Forms/MainForm.cs
+++ RomRaiderLogViewer/RomRaiderLogViewer/Forms/MainForm.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace RomRaiderLogViewer
 {
     public partial class MainForm : Form
     {
+        #region Private Fields
+
+        private string[] _columns;
+        private string[] _lines;
+
+        #endregion Private Fields
+
         #region Public Constructors
 
         public MainForm()
@@ -17,18 +28,16 @@ namespace RomRaiderLogViewer
 
         #region Events
 
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
-
         private void MainForm_Load(object sender, EventArgs e)
         {
             var version = Assembly.GetExecutingAssembly().GetName().Version;
             lblBuild.Text = $"{version.Build}.{version.Revision}";
+
+            cmbGridType.DataSource = Enum.GetValues(typeof(SeriesChartType)).Cast<SeriesChartType>().OrderBy(x => x.ToString()).ToList();
+            cmbGridType.SelectedItem = SeriesChartType.Line;
         }
 
-        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        private void Open_Click(object sender, EventArgs e)
         {
             using (var dialog = new OpenFileDialog())
             {
@@ -38,9 +47,106 @@ namespace RomRaiderLogViewer
                     return;
 
                 lblFileName.Text = dialog.SafeFileName;
+                _lines = File.ReadAllLines(dialog.FileName);
+
+                BuildGrid();
             }
         }
 
+        private void BuildGraph_Click(object sender, EventArgs e)
+        {
+            BuildGraph();
+        }
+
         #endregion Events
+
+        #region Private Methods
+
+        private void BuildGraph()
+        {
+            //Chart type
+            Enum.TryParse(cmbGridType.SelectedValue.ToString(), out SeriesChartType chartType);
+
+            //Check valid columns selected to graph
+            if (chartType == SeriesChartType.ThreeLineBreak && checkedListBox.CheckedItems.Count > 1)
+            {
+                MessageBox.Show("This chart type only accepts a single column", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            //Reset graph
+            chart.ResetAutoValues();
+            chart.Series.Clear();
+
+            //Build series for each selected column
+            foreach (var item in checkedListBox.CheckedItems)
+            {
+                var series = new Series
+                {
+                    Name = item.ToString(),
+                    IsVisibleInLegend = true,
+                    IsXValueIndexed = true,
+                    ChartType = chartType
+                };
+
+                chart.Series.Add(series);
+
+                foreach (var line in _lines)
+                {
+                    //Get the index of the column
+                    var index = Array.FindIndex(_columns, row => row == (string)item);
+                    var split = line.Split(',');
+
+                    if (double.TryParse(split[0], out double x))
+                    {
+                        //If no data exist for column set value to 0
+                        if (index < split.Length && double.TryParse(split[index], out double y))
+                            series.Points.AddXY(x, y);
+                        else
+                            series.Points.AddXY(x, 0);
+                    }
+                }
+            }
+
+            chart.Invalidate();
+        }
+
+        private void BuildGrid()
+        {
+            var dataTable = new DataTable();
+            _columns = new string[0];
+
+            //Clear check list
+            checkedListBox.Items.Clear();
+
+            //Check all lines to find which row has the most columns
+            foreach (var line in _lines)
+            {
+                var split = line.Split(',');
+
+                if (split.Length > _columns.Length)
+                    _columns = split;
+            }
+
+            //Add columns to table and check list
+            foreach (var column in _columns)
+            {
+                dataTable.Columns.Add(column);
+                checkedListBox.Items.Add(column);
+            }
+
+            //Add rows
+            foreach (var line in _lines)
+            {
+                var split = line.Split(',');
+
+                if (double.TryParse(split[0], out double x))
+                    dataTable.Rows.Add(split);
+            }
+
+            dataGridView1.DataSource = dataTable;
+        }
+
+        #endregion Private Methods
     }
 }
